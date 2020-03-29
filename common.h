11f8b97c44863a3b6a29ad16edc062700b9a3a81
avishf7 <62483176+avishf7@users.noreply.github.com>
2020-03-29 19:52:42 +0300

    Update common.h

--- common.h
+++ common.h
@@ -3,8 +3,8 @@
 
 //The function accepts 4 variables
 //The variable 4
-//Is a function that does some action on 2 variables and returns the result
-//Variables 3 and 4 accept the 2 variables that want to do some action on them
+//Is a function that does some action on 2 variables and returns the result in another
+//Variables 2 and 3 accept the 2 variables that want to do some action on them
 //The first variable Gets the result of the action
 void operetion(void*, const void*, const void*, void (*)(void*, const void*, const void*));
 
