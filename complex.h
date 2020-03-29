ec28c67e9b23f849368ace8d4f56f82039a471dc
eliialoz <62305714+eliialoz@users.noreply.github.com>
2020-03-29 15:56:33 +0300

    הוספתי הערות

--- complex.h
+++ complex.h
@@ -8,31 +8,62 @@ struct Complex
 	double real;
 	double imaginary;
 };
-
+//The function accepts 2 numbers and returns the complex number
+//If the user entered only one number, the imaginary value would be initialized in the 0
 Complex& ComplexCtor(double, double);
 
+//The function accepts rational numer and returns complex number 
+//The real part is equal to the rational number and the imaginary part is equal to 0
 Complex& ComplexCtor(Rational&);
 
+//The function accepts  a complex number and returns its length
 double abs(Complex&);
 
+//The function accepts 2 complex numbers and adds to each other
 void addComplex(void*, const void*, const void*);
 
+//The function accepts 2 complex numbers and returns one minus the other
 void subComplex(void*, const void*, const void*);
 
+//The function accepts 2  complex numbers and returns one double the other
 void subComplex(void*, const void*, const void*);
 
+//The function receives 2 complex numbers and compares them
+//If they are equal the function returns 0
+//If the first number is greater than the second number the function returns 1
+//If the first number is less than the second number the function returns - 1
 int compareComplex(const void*, const void*);
 
+//The function does operator overloading Of operation plus
 Complex operator + (const Complex&, const Complex&);
+
+//The function does operator overloading Of operation minus
 Complex operator - (const Complex&, const Complex&);
+
+//The function does operator overloading Of operation multiplication
 Complex operator * (const Complex&, const Complex&);
+
+//The function does operator overloading Of operation <
 Complex operator < (const Complex&, const Complex&);
+
+//The function does operator overloading Of operation >
 Complex operator > (const Complex&, const Complex&);
+
+//The function does operator overloading Of operation <=
 Complex operator <=(const Complex&, const Complex&);
+
+//The function does operator overloading Of operation >=
 Complex operator >=(const Complex&, const Complex&);
+
+//The function does operator overloading Of operation == Between the length of the composite numbers
 Complex operator ==(const Complex&, const Complex&);
+
+//The function does operator overloading Of operation != Between the length of the composite numbers
 Complex operator !=(const Complex&, const Complex&);
 
+//The function does operator overloading Of operation >>
 istream& operator >>(istream&, Complex&);
+
+//The function does operator overloading Of operation <<
 ostream& operator >>(ostream&, Complex&);
 #endif
--- rational.h
+++ rational.h
@@ -10,32 +10,61 @@ struct Rational
 	int numerator;
 	uint denominator;
 };
-
+//The function accepts 2 numbers and returns the rational number
+//If the user entered only one number, the rational value would be initialized in the denominator to 1
 Rational& rationalCtor(int numerator, uint denominator = 1);
 
 // the function returns the largest divider of the two numbers
 uint gcd(uint, uint);
 
-// the function returns the redced fraction
+// the function returns the reduced fraction
 Rational reductFraction(int, uint);
 
+//The function accepts 2 rational numbers and adds to each other
 void addRational(void*, const void*, const void*);
 
+//The function accepts 2 rational numbers and returns one minus the other
 void subRational(void*, const void*, const void*);
 
+//The function accepts 2 rational numbers and returns one double the other
 void multRational(void*, const void*, const void*);
 
+//The function receives 2 rational numbers and compares them
+//If they are equal the function returns 0
+//If the first number is greater than the second number the function returns 1
+//If the first number is less than the second number the function returns - 1
 int compareRational(const void*, const void*);
 
+//The function does operator overloading Of operation plus
 Rational operator + (const Rational&, const Rational&);
+
+//The function does operator overloading Of operation minus
 Rational operator - (const Rational&, const Rational&);
+
+//The function does operator overloading Of operation multiplication
 Rational operator * (const Rational&, const Rational&);
+
+//The function does operator overloading Of operation <
 Rational operator < (const Rational&, const Rational&);
+
+//The function does operator overloading Of operation >
 Rational operator > (const Rational&, const Rational&);
+
+//The function does operator overloading Of operation <=
 Rational operator <=(const Rational&, const Rational&);
+
+//The function does operator overloading Of operation >=
 Rational operator >=(const Rational&, const Rational&);
+
+//The function does operator overloading Of operation ==
 Rational operator ==(const Rational&, const Rational&);
+
+//The function does operator overloading Of operation !=
 Rational operator !=(const Rational&, const Rational&);
+
+//The function does operator overloading Of operation >>
 istream& operator >>(istream&, Rational&);
+
+//The function does operator overloading Of operation >>
 ostream& operator >>(ostream&, Rational&);
 #endif
