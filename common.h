e41a3af48c88614891590dfb0e7499db7c3ba221
eliialoz <62305714+eliialoz@users.noreply.github.com>
2020-03-29 17:31:42 +0300

    שינוי ב common.h

--- common.h
+++ common.h
@@ -1,7 +1,11 @@
 #ifndef COMMON
 #define COMMON
 
-
+//The function accepts 4 variables
+//The variable 4
+//Is a function that does some action on 2 variables and returns the result
+//Variables 3 and 4 accept the 2 variables that want to do some action on them
+//The first variable Gets the result of the action
 void operetion(void*, const void*, const void*, void (*)(void*, const void*, const void*));
 
 #endif
